22ead395bea04475354168e46bd37304deaf0c66
Sangmin Yoon <sangminyn@gmail.com>
2023-08-07 09:04:45 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,9 @@
+function solution(price) {
+    var answer = price;
+    
+    if(price >= 500000) answer = price * 0.8
+    else if(price >= 300000) answer = price * 0.9
+    else if(price >= 100000) answer = price * 0.95
+        
+    return Math.floor(answer)
+}
