b3f03cf966fe5b0360514f2e1d54f3e088c0e3f6
Sangmin Yoon <sangminyn@gmail.com>
2024-04-18 21:44:45 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 9340 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,39 @@
+const FILE_PATH = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let fs = require("fs");
+let input = fs.readFileSync(FILE_PATH).toString().trim().split("\n");
+
+const alphabet = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+  e: 5,
+  f: 6,
+  g: 7,
+  h: 8,
+  i: 9,
+  j: 10,
+  k: 11,
+  l: 12,
+  m: 13,
+  n: 14,
+  o: 15,
+  p: 16,
+  q: 17,
+  r: 18,
+  s: 19,
+  t: 20,
+  u: 21,
+  v: 22,
+  w: 23,
+  x: 24,
+  y: 25,
+  z: 26,
+};
+
+let sum = 0;
+for (let i = 0; i < Number(input[0]); i++) {
+  sum += alphabet[input[1][i]] * 31 ** i;
+}
+
+console.log(sum);
