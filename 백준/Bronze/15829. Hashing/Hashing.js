d7f2ddfc08e164c1cb1eadf55bc4f2b73f614429
Sangmin Yoon <sangminyn@gmail.com>
2024-04-18 22:02:15 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 9340 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -3,37 +3,38 @@ let fs = require("fs");
 let input = fs.readFileSync(FILE_PATH).toString().trim().split("\n");
 
 const alphabet = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
-  e: 5,
-  f: 6,
-  g: 7,
-  h: 8,
-  i: 9,
-  j: 10,
-  k: 11,
-  l: 12,
-  m: 13,
-  n: 14,
-  o: 15,
-  p: 16,
-  q: 17,
-  r: 18,
-  s: 19,
-  t: 20,
-  u: 21,
-  v: 22,
-  w: 23,
-  x: 24,
-  y: 25,
-  z: 26,
+  a: 1n,
+  b: 2n,
+  c: 3n,
+  d: 4n,
+  e: 5n,
+  f: 6n,
+  g: 7n,
+  h: 8n,
+  i: 9n,
+  j: 10n,
+  k: 11n,
+  l: 12n,
+  m: 13n,
+  n: 14n,
+  o: 15n,
+  p: 16n,
+  q: 17n,
+  r: 18n,
+  s: 19n,
+  t: 20n,
+  u: 21n,
+  v: 22n,
+  w: 23n,
+  x: 24n,
+  y: 25n,
+  z: 26n,
 };
 
-let sum = 0;
+let sum = 0n;
 for (let i = 0; i < Number(input[0]); i++) {
-  sum += alphabet[input[1][i]] * 31 ** i;
+  const idx = BigInt(i);
+  sum += alphabet[input[1][i]] * 31n ** idx;
 }
 
-console.log(sum);
+console.log(String(sum % 1234567891n));
