b8355142dd638210c6864d698b8239247f908cd1
Sangmin Yoon <sangminyn@gmail.com>
2024-05-26 18:12:31 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 9956 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.js
@@ -1,12 +1,25 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const iterator = input[Symbol.iterator]();
-let T = +iterator.next().value;
-while (T--) {
-  const wear = {};
-  let n = +iterator.next().value;
-  while (n--) {
-    const [ _, kind ] = iterator.next().value.split(/\s+/);
-    wear[kind] = (wear[kind] ?? 0) + 1;
+const FILE_PATH = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let fs = require("fs");
+let input = fs.readFileSync(FILE_PATH).toString().trim().split("\n");
+
+const T = +input.shift();
+
+let N = 0;
+
+for (let i = 0; i < T; i++) {
+  const M = +input[N];
+  const obj = {};
+  for (let j = N + 1; j < M + N + 1; j++) {
+    const [옷, 종류] = input[j].split(" ");
+    if (!obj[종류]) obj[종류] = [];
+    obj[종류].push(옷);
   }
-  console.log([...Object.values(wear)].reduce((acc, v) => acc*(v+1), 1) - 1);
+
+  let sum = 1;
+  for (const x in obj) {
+    sum *= obj[x].length + 1;
+  }
+  console.log(sum - 1);
+
+  N += M + 1;
 }
